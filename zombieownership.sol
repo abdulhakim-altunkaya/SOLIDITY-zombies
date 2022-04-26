a42a02915049b8492aaa8797f56ff77f897e0b16
abdulhakim_altunkaya <altunkaya2@gmail.com>
2022-04-26 23:06:44 +0300

    zombie token functions added

--- zombieownership.sol
+++ zombieownership.sol
@@ -1,9 +1,34 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./zombieattack.sol";
-// Import file here
+import "./erc721.sol";
 
-// Declare ERC721 inheritance here
-contract ZombieOwnership is ZombieAttack {
+contract ZombieOwnership is ZombieAttack, ERC721 {
+
+  // 1. Define mapping here
+
+  function balanceOf(address _owner) external view returns (uint256) {
+    return ownerZombieCount[_owner];
+  }
+
+  function ownerOf(uint256 _tokenId) external view returns (address) {
+    return zombieToOwner[_tokenId];
+  }
+
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to]++;
+    ownerZombieCount[_from]--;
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
+  }
+
+  function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+    // 2. Add the require statement here
+    // 3. Call _transfer
+  }
+
+  function approve(address _approved, uint256 _tokenId) external payable {
+
+  }
 
 }
